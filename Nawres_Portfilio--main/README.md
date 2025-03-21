882f2823eb35072e3ce2f0a02543135123579922
Naoures  Hamrouni <74150824+Nawres2020@users.noreply.github.com>
2025-03-21 19:58:01 +0100

    Add files via upload

---
+++ Nawres_Portfilio--main/README.md
@@ -0,0 +1,65 @@
+# 🌐 Portfolio Nawres
+
+Welcome to my personal portfolio website! This is a fully responsive and modern portfolio built using HTML, CSS, and JavaScript to showcase my work and projects.
+
+# 📌 Features
+
+- 🎨 Modern and responsive design
+
+- 🌙 Dark and light theme support
+
+- 🔥 Smooth animations and interactive UI
+
+- 📄 Dynamic sections: About, Projects, Articles
+
+- ⚡ Optimized for performance and SEO
+
+# 🚀 Live Demo
+
+👉 View Live Website : https://dulcet-pony-aedfcc.netlify.app/
+
+# 🏧 Technologies Used
+
+- HTML5 for structured content
+
+- CSS3 (with custom styles & animations)
+
+- JavaScript for interactivity
+
+
+# 💂️ Project Structure
+
+portfolio_nawres/
+│── index.html        # Main landing page
+│── about.html        # About me section
+│── articles.html     # Blog or articles page
+│── projects.html     # Showcasing projects
+│── static/           # Assets folder
+│   ├── css/         # Stylesheets
+│   ├── js/          # JavaScript files
+│   ├── images/      # Media assets
+│── .gitignore       
+│── README.md        #  documentation
+
+# 🛠️ Installation & Usage
+
+To run this project locally, follow these steps:
+
+1/Clone the repository
+
+2/git clone https://github.com/Nawres2020/portfolio_nawres.git
+-cd portfolio_nawres
+
+-Open index.html in your browser
+
+-You can simply double-click the index.html file. Or, use a local server:
+
+3/Then open http://localhost:8000/ in your browser.
+
+🐜 License
+
+This project is licensed under the MIT License – feel free to use and modify it!
+
+🤝 Contributing
+
+Want to improve this portfolio? Feel free to fork this repo and submit a pull request. All contributions are welcome!
---
+++ Nawres_Portfilio--main/my_website/static/js/index.js
@@ -0,0 +1,152 @@
+const root = document.querySelector('html');
+const body = document.querySelector('body');
+const mainNav = document.querySelector("nav");
+const menu = document.querySelector('.nav-links');
+const menuButton = document.getElementById('menu-display');
+const themeDisplay = document.getElementById('theme-display');
+const themeContainer = document.querySelector('.theme-container');
+const themeSelectors = document.getElementsByClassName('theme-select');
+
+mainNav.classList.add('js-nav');
+
+const getTheme = () => {
+  const theme = localStorage.getItem('theme');
+  theme && setActiveSelector(theme);
+  root.className = theme;
+  const shade = getComputedStyle(document.documentElement).getPropertyValue('--shade-100');
+  document.querySelector('meta[name="theme-color"]').setAttribute('content', shade);
+}
+
+const setTheme = (className) => {
+  var root = document.getElementsByTagName('html')[0];
+  root.className = className;
+  localStorage.setItem('theme', className);
+  const shade = getComputedStyle(document.documentElement).getPropertyValue('--shade-100');
+  document.querySelector('meta[name="theme-color"]').setAttribute('content', shade);
+  setActiveSelector(className);
+}
+
+const setActiveSelector = (className) => {
+  var selectedTheme = document.getElementById(`${className}-select`);
+  [...themeSelectors].forEach(item => {
+    item.classList.remove('active')
+  });
+  selectedTheme.classList.add('active');
+  hideThemeContainer();
+}
+
+const showThemeContainer = () => {
+  themeContainer.classList.add('visible');
+  [...themeSelectors].forEach(item => {
+    item.tabIndex = 0
+  });
+}
+
+const hideThemeContainer = () => {
+  themeContainer.classList.remove('visible');
+  [...themeSelectors].forEach(item => {
+    item.tabIndex = -1
+  });
+}
+
+const showMenu = () => {
+  menu.classList.add('visible');
+  menuButton.classList.add('active');
+}
+
+const hideMenu = () => {
+  menu.classList.remove('visible');
+  menuButton.classList.remove('active');
+}
+
+let previousScrollPosition = 0;
+
+const isScrollingDown = () => {
+  let scrolledPosition = window.scrollY;
+  let isScrollDown;
+
+  if (scrolledPosition > previousScrollPosition) {
+    isScrollDown = true;
+  } else {
+    isScrollDown = false;
+  }
+  previousScrollPosition = scrolledPosition;
+  return isScrollDown;
+}
+
+const handleNavScroll = () => {
+  if (mainNav.classList.contains('visible')) {
+    if (isScrollingDown()) {
+      mainNav.classList.add('scroll-down');
+      mainNav.classList.remove('scroll-up')
+    } else {
+      mainNav.classList.add('scroll-up');
+      mainNav.classList.remove('scroll-down')
+    }
+  } else {
+    mainNav.classList.remove('scroll-up');
+    mainNav.classList.remove('scroll-down')
+  }
+}
+
+getTheme();
+
+themeDisplay.addEventListener("click", function () {
+  hideMenu()
+  if (themeContainer.classList.contains('visible')) {
+    hideThemeContainer();
+  } else {
+    showThemeContainer();
+  }
+})
+
+menuButton.addEventListener("click", function () {
+  hideThemeContainer();
+  if (menu.classList.contains('visible')) {
+    hideMenu();
+  } else {
+    showMenu();
+  }
+})
+
+menu.addEventListener("click", function () {
+  hideThemeContainer();
+  hideMenu()
+})
+
+window.addEventListener('scroll', () => {
+  handleNavScroll()
+});
+
+
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    typeText();
+  })
+
+  function typeText() {
+    const text = "Machine Learning & Computer Vision Engineer | Full-Stack Developer";
+    const element = document.getElementById("animate-text-1");
+    let index = 0;
+
+    // Clear the element's content to prevent duplication
+    element.textContent = "";
+
+    function type() {
+      if (index < text.length) {
+        element.textContent += text.charAt(index); // Add one character at a time
+        index++;
+        setTimeout(type, 100); // Adjust 100ms to change typing speed
+      }
+    }
+
+    type();
+  }
+
+
+
+
+
+
+
+
